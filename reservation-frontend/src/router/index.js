e3efe22b7bf0f19fc160c16887e608cfc11c1d18
canbulgay <canbulgay@outlook.com>
2022-03-08 18:45:53 +0300

    Create routes.js for seperate routes in index.js

--- reservation-frontend/src/router/index.js
+++ reservation-frontend/src/router/index.js
@@ -1,60 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Reservation from "../views/Reservation.vue";
-import Register from "../views/Register.vue";
-import Login from "../views/Login.vue";
-import Logout from "../views/Logout.vue";
-import Profile from "../views/Profile.vue";
 import store from "../store";
+import routes from "./routes";
 
 Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
-  {
-    path: "/form",
-    name: "Reservation",
-    component: Reservation,
-  },
-  {
-    path: "/register",
-    name: "Register",
-    component: Register,
-    meta: { requiresVisitor: true },
-  },
-  {
-    path: "/login",
-    name: "Login",
-    component: Login,
-    meta: { requiresVisitor: true },
-  },
-  {
-    path: "/logout",
-    name: "Logout",
-    component: Logout,
-    meta: { requiresAuth: true },
-  },
-  {
-    path: "/profile",
-    name: "Profile",
-    component: Profile,
-    meta: { requiresAuth: true },
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
-];
 
 const router = new VueRouter({
   mode: "history",
